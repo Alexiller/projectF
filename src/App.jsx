f225b3d5562bb524d3e0e53a5fb97208cb62eafc
Alexiller <112704627+Alexiller@users.noreply.github.com>
2023-02-20 19:59:12 +0100

    interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Center, Flex, Heading, Spacer } from "@chakra-ui/react"
 import Footer from "./Layout/Footer"
 import Header from "./Layout/Header"
 import Navbar from "./Layout/Navbar"
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,14 +1,18 @@
-import { Avatar, Center, Flex, Heading, Spacer, Text } from '@chakra-ui/react'
+import { MoonIcon, PhoneIcon } from '@chakra-ui/icons'
+import { Avatar, Center, Flex, Heading, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 function Header() {
   return (
     <Flex bgColor={'green'} p={2}>
+      <Avatar name='Alex Des' src='https://bit.ly/dan-abramov' />
     <Center>
       <Heading as='h1' size={'xl'}>projectF</Heading>
     </Center>
     <Spacer />
-    <Avatar name='Alex Des' src='https://bit.ly/dan-abramov' />
+    <Center w='30px' mx={'5'}>
+      <MoonIcon />
+    </Center>
   </Flex>
   )
 }
